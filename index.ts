e607c72efff85d046949a44def335ddc2b667434
Nathan Hare <me@laspruca.nz>
2021-12-26 22:08:52 +1300

    Add some more comments

--- index.ts
+++ index.ts
@@ -1,7 +1,15 @@
+/** Stores a reference to a HTML element, and allows for centering on it */
 export class Div {
   private element: HTMLElement;
 
+  /** Construct a new Div
+   * @param id: If `is` is a string, the library will attempt to find the element using `document.getElementById`.
+   * If `id` is an HTMLElement, then it will set the element to `id`
+   *
+   * @throws If `document.getElementById` returns null
+   */
   constructor(id: string | HTMLElement) {
+    // Check to see if string, and get the element if it is
     if (typeof id == "string") {
       let elm = document.getElementById(id);
       if (elm !== null) {
@@ -9,12 +17,16 @@ export class Div {
       } else {
         throw new TypeError(`Can not find element with ID ${id}`);
       }
+      // If it aint a string, assume HTMLElement and set this.element to it
     } else {
       this.element = id;
     }
   }
 
-  center() {
+  /** Centers your div
+   * @param className An optional class to give to the parent element responsable for centering your div
+   */
+  center(className?: string) {
     // Get the parent node
     let parent = this.element.parentNode ?? document.getRootNode();
 
@@ -29,5 +41,10 @@ export class Div {
     // Setup parenting correctly
     parent.appendChild(div);
     div.appendChild(this.element);
+
+    // Add optional class name
+    if (typeof className === "string") {
+      div.setAttribute("class", className);
+    }
   }
 }
